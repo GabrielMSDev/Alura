40df914bc9cad1c296fe008e04b671893b4506b5
unknown <gabrielms1612@gmail.com>
2023-05-30 20:21:57 -0300

    Adicionando 7 Days of Code - Dia 2

---
+++ 7 Days Of Code - JS/Day 2/js/main.js
@@ -0,0 +1,23 @@
+const nome = prompt("Qual o seu nome?");
+const idade = prompt("Quantos anos você tem?");
+const linguagem = prompt("Qual linguagem de programação você está estudando?");
+
+const resumo = `Olá ${nome}, você tem ${idade} anos e já está aprendendo ${linguagem}!`;
+
+alert(resumo);
+
+const satisfacao = prompt(`Você gosta de estudar ${linguagem}? Responda com o número 1 para SIM ou 2 para NÃO.`);
+
+switch (satisfacao) {
+    case "1":
+        alert("Muito bom! Continue estudando e você terá muito sucesso.")
+        break;
+
+    case "2":
+        alert("Ahh que pena... Já tentou aprender outras linguagens?");
+        break;
+
+    default:
+        alert("Opção incorreta, por favor, recarregue a página e tente novamente!");
+        break;
+}
